d7344c1c896a4a5ac4a6b7ace4acc401c1e58203
Razi <razibhatti1@gmail.com>
2020-01-29 03:37:09 +0500

    served

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
-$('.carousel').carousel({
-    interval: 2000,
-    pause:'hover'
+let api = 'https://www.reddit.com/r/nsfw/search.json?q=nsfw&sort=top&limit=1'
+
+window.addEventListener('load', () => {
+    $('.carousel').carousel({
+        interval: 2000,
+        pause: 'hover'
+    })
+    fetch(api)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            data.data.children.forEach(element => {
+                console.log(element.data)
+            });
+        })
 })
