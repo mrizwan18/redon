1c717fbf212360b55a3c079c6ef39a7c1ccf325f
Razi <razibhatti1@gmail.com>
2020-02-01 22:22:40 +0500

    fixed mobile view

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,6 @@ window.addEventListener('load', () => {
             })
             .then(data => {
                 for (const element of data.data.children) {
-                    console.log(i)
                     let share = "reddit.com" + element.data.permalink;
                     $('<div class="carousel-item"><div class="post" id="post' + i + '"><div class="overlay"><div class= "items"></div><div class="items head"><p>' + element.data.title + '</p></div><div class="items details"><p>By: <span id="posted-by">' + element.data.author + '</span></p><p>In: <span id="posted-in">' + element.data.subreddit + '</span></p> <p class="upvotes"><i class="fas fa-arrow-alt-circle-up"></i><span>' + element.data.ups + '</span></p></div><div class="items share"><a href="' + share + '"><i class="fas fa-share"></i></a></div></div ></div >  </div>').appendTo('.carousel-inner');
                     $('<li data-target="#post-carousel" data-slide-to="' + i + '"></li>').appendTo('.carousel-indicators')
